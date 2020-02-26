a0d9c4f53d21aa55cffade38d643dec1d465bac1
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-02-27 01:29:44 +0300

    feat: solve typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+function min (array) {
+    if (arguments.length == 0 || array.length == 0){
+        return 0;    
+    }
+    return array.sort((a, b) => a-b)[0];
 }
+console.log(min([3, 2, 1]));
+exports.min = min;
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length == 0 || array.length == 0){
+        return 0;    
+    }
+    return array.sort((a, b) => a-b)[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length == 0 || array.length == 0){
+        return 0;    
+    }
+    let avg = array.reduce((sum, item) => sum + item, 0) / array.length;
+    return avg;
 }
